13bd7ebf716d0c13a874a3c4b9ca4596273a177b
Rodrigo Avancini <r23111@hotmail.com>
2020-12-02 13:43:28 -0300

    add comments to day 2 python

--- Day 2/day_2.py
+++ Day 2/day_2.py
@@ -1,7 +1,6 @@
 f = open("./input.txt", 'r')
 
 file = f.readlines()
-passwords = []
 
 valid_passwords_part1 = 0
 valid_passwords_part2 = 0
@@ -13,14 +12,20 @@ for i in range(len(file)):
     p = [temp[0], temp1[0], temp1[1].replace(':', ''), temp1[2]]
 
     repetitions = 0
+
+    # part 1: count the number of repetitions of the letter (p[2]) on the password (p[3]),
+    # and check if it pass the requirements (p[0] <= n <= p[1])
     for l in p[3]:
         if(l == p[2]):
             repetitions += 1
     if repetitions >= int(p[0]) and repetitions <= int(p[1]):
         valid_passwords_part1 += 1
 
-    if((p[3][int(p[0]) - 1] == p[2] and not p[3][int(p[1]) - 1] == p[2]) or (p[3][int(p[1]) - 1] == p[2] and not p[3][int(p[0]) - 1] == p[2])):
+    # part 2: check if the letter is on (only) one of the indeces -1 (the array starts at 1 -> p[0] - 1 and p[1] - 1)
+    if((p[3][int(p[0]) - 1] == p[2] and not p[3][int(p[1]) - 1] == p[2]) or
+       (p[3][int(p[1]) - 1] == p[2] and not p[3][int(p[0]) - 1] == p[2])):
         valid_passwords_part2 += 1
 
 f.close()
+
 print(f"part 1: {valid_passwords_part1} \npart 2: {valid_passwords_part2}")
