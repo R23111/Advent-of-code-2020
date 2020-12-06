e8d8d93caee986eb00887ff1caeb56a4dc6f4b54
Rodrigo Avancini <r23111@hotmail.com>
2020-12-06 16:14:28 -0300

    updated day_6.py

--- Day 6/day_6.py
+++ Day 6/day_6.py
@@ -1,10 +1,5 @@
 file = open("./input.txt").read().strip().split("\n\n")
 questions = "abcdefghijklmnopqrstuvwxyz"
 count_pt1, count_pt2 = 0, 0
-
-for cust in file:
-    temp = cust.split("\n")
-    count_pt2 += len(set.intersection(*map(set, temp)))
-    count_pt1 += len(set.union(*map(set, temp)))
-    
-print(sum(len(set.intersection(*map(set, x.split("\n")))) for x in file))
+print("Part 1: " + str(sum(len(set.union(*map(set, x.split("\n")))) for x in file)))
+print("Part 2: " + str(sum(len(set.intersection(*map(set, x.split("\n")))) for x in file)))
